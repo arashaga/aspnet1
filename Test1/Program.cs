1867f3955756b0f7b07cfc6a8c82b2ffad1b75a2
Mosharraf <amosharrafolnolk@utsystem.edu>
2018-07-25 15:43:35 -0500

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Test1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+        // no more web.config
+        public static IWebHost BuildWebHost(string[] args) =>
+            // configuring the web server environmnet
+            // use Kestrel webserver
+            // IIS integration
+            //sets up Logging
+            //IConfiguration service made avaialble reading JSON file
+            //
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
